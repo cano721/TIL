1fe7b0e8b110f30c37ec2412c258859ccc58abeb
cano721 <cano721@naver.com>
2021-11-13 20:09:18 +0900

    spring 컴포넌트 스캔 공부

--- work/javaSpring/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ work/javaSpring/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     // 동시성 이슈가 생길 수 있다. -> 추후엔 컨커런트 해쉬맵? 이란걸 써야함
