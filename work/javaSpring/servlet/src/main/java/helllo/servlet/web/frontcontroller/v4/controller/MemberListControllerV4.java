9eb7a42c4364ceb2756a260c5a0db30280883de5
cano721 <cano721@naver.com>
2021-12-01 19:32:27 +0900

    스프링 - MVC 프레임워크 만들기(어댑터)

---
+++ work/javaSpring/servlet/src/main/java/helllo/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,23 @@
+package helllo.servlet.web.frontcontroller.v4.controller;
+
+import helllo.servlet.domain.member.Member;
+import helllo.servlet.domain.member.MemberRepository;
+import helllo.servlet.web.frontcontroller.ModelView;
+import helllo.servlet.web.frontcontroller.v3.ControllerV3;
+import helllo.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+        model.put("members",members);
+        return "members";
+    }
+
+}
