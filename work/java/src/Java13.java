742a03f28083e12dc4d9037e22cf12b52c15179e
계해범 <cano721@naver.com>
2024-10-06 21:24:31 +0900

    Java 13 중요 변경 사항 정리

---
+++ work/java/src/Java13.java
@@ -0,0 +1,22 @@
+/**
+ * Java 13
+ *
+ * textBlocks
+ *
+ * java 13 변경사항 정리 글 https://cano721.tistory.com/234
+ */
+public class Java13 {
+    public static void main(String[] args) {
+        textBlock();
+    }
+
+    private static void textBlock() {
+        String textBlock = """
+                first text
+                second text
+                final text
+                """;
+
+        System.out.println(textBlock);
+    }
+}
